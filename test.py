ac7f9cee7a571b7c1440075d896eceb88f6d6d05
Thomas Wittek <thomas@wittek.me>
2020-05-23 21:31:10 +0200

    Add basic Model test.

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import datetime
+
+import model
+import qif
+
+
+transactions = (
+    model.Transaction(datetime.datetime.now(), 1000, 'Payee1', 'Memo1', 'Cat1'),
+    model.Transaction(datetime.datetime.now(), -300, 'Payee2', 'Memo2', 'Cat2'),
+    model.Transaction(datetime.datetime.now(), -200, 'Payee3', 'Memo3', 'Cat3'),
+    model.Transaction(datetime.datetime.now(), -100, 'Payee4', 'Memo4', 'Cat4'),
+)
+
+account = model.Account('Test', transactions)
+
+print(qif.serialize_account(account))
